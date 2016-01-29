77270baa6378288017d645ddeb501e9ae97e81e3
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-01-29 13:18:33 -0800

    completed minimum work for self-assessment. will work on extra assignments

--- sum-array/sumArray.js
+++ sum-array/sumArray.js
@@ -7,7 +7,21 @@ example:
 var arr = [1, 9, 7, 15];
 
 sumArray(arr) --> 32;
+*/
 
+var sumArray = function(arr){
+	var sum = 0;
+	for(var i = 0; i < arr.length; i++){
+		sum += arr[i]
+	}
+	return sum;
+};
+
+var arr = [1, 2, 3, 4];
+debug(sumArray(arr));
+
+
+/*
 
 *EXTRA* build an 'add' function that takes two numbers as parameters and adds them together. Replace your add functionality inside of sumArray with your new 'add' function.
 
