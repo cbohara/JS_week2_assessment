e59bf1ffeb59a2abfba87f9a59d3db8a2701bf8e
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-01-29 13:20:35 -0800

    completed self-assessment minimums but want to work on extras

--- sum-array/sumArray.js
+++ sum-array/sumArray.js
@@ -9,17 +9,16 @@ var arr = [1, 9, 7, 15];
 sumArray(arr) --> 32;
 */
 
+//create function that takes an array as an parameter
 var sumArray = function(arr){
+	//initialize the counter to zero
 	var sum = 0;
+	//loop through each index within the array and add the array index value to the sum total
 	for(var i = 0; i < arr.length; i++){
 		sum += arr[i]
 	}
 	return sum;
 };
-
-var arr = [1, 2, 3, 4];
-debug(sumArray(arr));
-
 
 /*
 
