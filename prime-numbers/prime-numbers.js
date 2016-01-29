b877577e2ea1fee9741e3e1a78ceab41e036cc39
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-01-29 11:31:59 -0800

    add prime numbers file

---
+++ prime-numbers/prime-numbers.js
@@ -0,0 +1,32 @@
+/*
+
+1.Define the rules of the problem 
+	determine if the input value is a prime number
+	a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself
+2.Determine inputs and outputs
+	input: test value
+	output: prime number is true, non-prime is false
+3.Explore the problem and discover techniques or patterns that might be useful
+	what algorthims are already out there to determine if a number is prime or not?
+	does JS have a built in function?
+4.Generate a simple plan that should solve the problem
+5.Turn that plan into steps- put your pseudocode skills to use!
+
+example: 
+
+isPrime(13) --> true;
+isPrime(15) --> false;
+
+
+
+*EXTRA* expand your isPrime to an isPrimeArray function to loop through an array of numbers, collect the prime numbers, and return them to the user. If there are no prime numbers in the array, isPrimeArray returns 'there are no primes';
+
+var primeArray = [7, 14, 27, 671]; 
+
+var nonPrimeArray = [9, 15, 20]
+
+isPrimeArray(primeArray); ---> [7, 671];
+isPrimeArray(nonPrimeArray) ---> 'there are no primes';
+
+*/
+
